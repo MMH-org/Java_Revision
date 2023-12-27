defc1c36788fbaad9b908cd458d304eaf7b2aaae
Mohsin <mdmohsinhaider6@gmail.com>
2023-12-28 01:56:13 +0530

    Typecast

--- src/demo.java
+++ src/demo.java
@@ -1,4 +1,15 @@
 package src;
 
+import java.util.Scanner;
+
 public class demo {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        char ch = 'a';
+        System.out.println(ch);
+
+        int x =(int) ch; // typeCast
+        System.out.println(x);
+    }
 }
