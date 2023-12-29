b3c6717077aa3ceaf23c3df24dc49f60124d2bc3
Mohsin <mdmohsinhaider6@gmail.com>
2023-12-29 23:35:21 +0530

    Add Question

---
+++ src/DayQ.java
@@ -0,0 +1,16 @@
+package src;
+
+
+
+class Parent{}
+class Child extends Parent{
+    public void hello(){
+        System.out.println("hello");
+    }
+}
+public class DayQ {
+    public static void main(String[] args) {
+        Parent child=new Child();
+        child.hello(); // find Answer hello, Compiler error, and Runtime exception
+    }
+}
