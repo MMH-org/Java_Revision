4776653581ee22101915840dbbe4902fb4bdfde3
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-01 19:50:47 +0530

    Solution

---
+++ src/SolutionJava.java
@@ -0,0 +1,11 @@
+package src;
+
+public class SolutionJava {
+    public static void main(String[] args) {
+
+        byte num =127;
+        num++;
+        num++;
+        System.out.println(num);
+    }
+}
