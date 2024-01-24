bad41bb447e29f596c4ba7b76ff98969478469bd
Mohsin <mdmohsinhaider6@gmail.com>
2024-01-25 02:38:01 +0530

    Input Output of function next(); and nextLine();

---
+++ src/Java/InPutOutPut.java
@@ -0,0 +1,19 @@
+package src.Java;
+
+import java.sql.SQLOutput;
+import java.util.Scanner;
+
+public class InPutOutPut {
+    public static void main(String[] args) {
+        Scanner sc_object_1=new Scanner(System.in); // creating an object of Scanner class
+        Scanner sc_object_2=new Scanner(System.in); // creating an object of Scanner class
+
+        System.out.print("Enter string next() In Put :  ");
+        String name_1= sc_object_1.next();
+        System.out.println("next() function out Put is :  "+name_1);
+
+        System.out.print("Enter string nextLine() Input :  ");
+        String name_2 = sc_object_2.nextLine();
+        System.out.println("nextLine() Function Out Put is :  "+name_2);
+    }
+}
--- src/SolutionJava.java
+++ src/SolutionJava.java
@@ -1,11 +1,10 @@
 package src;
-
 public class SolutionJava {
     public static void main(String[] args) {
 
         byte num =127;
         num++;
         num++;
-        System.out.println(num);
+        System.out.println(0);
     }
 }
