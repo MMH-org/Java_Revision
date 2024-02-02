2f68c1ea29ef942b87ac1d0379f53b628acfdf68
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-03 00:12:04 +0530

    Continue keyword

---
+++ src/JavaBasic/Continue.java
@@ -0,0 +1,14 @@
+package src.JavaBasic;
+
+public class Continue {
+    public static void main(String[] args) {
+        //for loop
+        for(int i=1;i<=10;i++){
+            if(i==5){
+                //using a continue statement
+                continue;//it will skip the rest statement
+            }
+            System.out.print(i +" ");
+        }
+    }
+}
