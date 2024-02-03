5897608687da8f9bdf5cacca2d1ec7fcce63db22
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-03 23:37:52 +0530

    Anonymous Array in Java

--- src/JavaBasic/Arrays/AnonymousArray.java
+++ src/JavaBasic/Arrays/AnonymousArray.java
@@ -4,11 +4,11 @@ package src.JavaBasic.Arrays;
 //to method.
 public class AnonymousArray{
     //creating a method which receives an array as a parameter
-    static void printArray(int arr[]){
+    static void printArray(int[] arr){
         for(int i=0;i<arr.length;i++)
             System.out.println(arr[i]);
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
         printArray(new int[]{10,22,44,66});//passing anonymous array to method
     }}
