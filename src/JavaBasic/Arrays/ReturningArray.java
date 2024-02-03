f381914951ebc3019547614e31718016a426aeb9
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-03 23:43:40 +0530

    Returning Array from the Method

--- src/JavaBasic/Arrays/ReturningArray.java
+++ src/JavaBasic/Arrays/ReturningArray.java
@@ -6,10 +6,10 @@ public class ReturningArray{
         return new int[]{10,30,50,90,60};
     }
 
-    public static void main(String args[]){
+    public static void main(String[] args){
 //calling method which returns an array
-        int arr[]=get();
+        int[] arr =get();
 //printing the values of an array
         for(int i=0;i<arr.length;i++)
-            System.out.println(arr[i]);
+            System.out.print(arr[i]);
     }}
