37feaf7d4e2293fb56f9d4c1a5b2d35f92c7b397
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-03 23:28:59 +0530

    arrays init and use  in java

--- src/JavaBasic/Arrays/Array1.java
+++ src/JavaBasic/Arrays/Array1.java
@@ -5,5 +5,5 @@ class Array1{
         int a[]={33,3,4,5};//declaration, instantiation and initialization
 //printing array
         for(int i=0;i<a.length;i++)//length is the property of array
-            System.out.println(a[i]);
+            System.out.print(a[i]);
     }}
