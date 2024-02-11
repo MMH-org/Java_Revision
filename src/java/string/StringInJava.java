897e75a4d26707c079e8e2d33c95e3a5569bdea3
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-11 12:15:16 +0530

    string

--- src/java/string/StringInJava.java
+++ src/java/string/StringInJava.java
@@ -5,5 +5,11 @@ public class StringInJava {
         String str = "md mohsin haider";
         System.out.println(str.length());
         System.out.println(str);
+
+        // string objects
+        String obj_Str= new String("Devil");
+        System.out.println(obj_Str.toLowerCase());
+        System.out.println(obj_Str.codePointBefore(2));
+
     }
 }
