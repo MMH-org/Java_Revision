d1aff603920d2a0351a5078d7f2492ea846cee0e
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-11 12:03:24 +0530

    string and its lenghts

---
+++ src/java/string/StringInJava.java
@@ -0,0 +1,9 @@
+package src.java.string;
+
+public class StringInJava {
+    public static void main(String[] args) {
+        String str = "md mohsin haider";
+        System.out.println(str.length());
+        System.out.println(str);
+    }
+}
