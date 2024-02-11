3ebe90c0af0b3a78c566dd6e0a946e1ba000b426
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-11 12:18:10 +0530

    string StringBuffer

---
+++ src/java/string/StringBufferJava.java
@@ -0,0 +1,8 @@
+package src.java.string;
+
+public class StringBufferJava {
+    public static void main(String[] args) {
+        StringBuffer demoString = new StringBuffer("Md Mohsin Haider");
+        System.out.println(demoString);
+    }
+}
