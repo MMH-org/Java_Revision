df12e5edf2b1a166891bebd474581646fd1ceda5
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-17 11:42:42 +0530

    String using new keyword

---
+++ src/java/string/CreatingString/UsingnewKeyword.java
@@ -0,0 +1,8 @@
+package src.java.string.CreatingString;
+
+public class UsingnewKeyword {
+    public static void main(String[] args) {
+        String str_obj=new String("Md Mohsin Haider");
+        System.out.println(str_obj);
+    }
+}
