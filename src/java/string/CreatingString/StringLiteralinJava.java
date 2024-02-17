71e3e439d7a43ec05870a1ffb07df15a6122e1a8
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-17 11:39:20 +0530

    string Literal in java

---
+++ src/java/string/CreatingString/StringLiteralinJava.java
@@ -0,0 +1,8 @@
+package src.java.string.CreatingString;
+
+public class StringLiteralinJava  {
+    public static void main(String[] args) {
+        //    <String_Type> <string_variable> = "<sequence_of_string>";
+        String stringDemo="this is way of define String Literal";
+    }
+}
