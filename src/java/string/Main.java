6aa0979ba0fa6b9a9bd7c6c87d9269035ce7b9b6
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-19 22:30:43 +0530

    String Formating in java

--- src/java/string/Main.java
+++ src/java/string/Main.java
@@ -1,4 +1,24 @@
 package src.java.string;
 
+import java.util.Scanner;
+
 public class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        System.out.println("================================");
+        for(int i=0;i<3;i++) {
+            String s1=sc.next();
+            int x=sc.nextInt();
+            //Complete this line
+
+            // Format the string column
+            System.out.printf("%-15s", s1);
+
+            // Format the integer column with leading zeros if necessary
+            System.out.printf("%03d%n", x);
+        }
+        sc.close();
+        System.out.println("================================");
+    }
 }
+
