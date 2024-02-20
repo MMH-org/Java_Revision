95efd31cd9d702fee8554e543ce41229b1de0bec
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-20 16:05:14 +0530

    print Pattern With help of \n

---
+++ src/ProgrammingChallenge/BackSlacNTypePattern.java
@@ -0,0 +1,8 @@
+package src.ProgrammingChallenge;
+
+public class BackSlacNTypePattern {
+    public static void main(String[] args) {
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+    }
+}
