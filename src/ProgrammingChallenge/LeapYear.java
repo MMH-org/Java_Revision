f506461b4a67e89a6613ed0997ec4d5850839302
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-20 23:00:02 +0530

    leap year

--- src/ProgrammingChallenge/LeapYear.java
+++ src/ProgrammingChallenge/LeapYear.java
@@ -8,7 +8,7 @@ public class LeapYear {
         System.out.print("Enter the Year which is leap year or Not ? :  ");
         int num = sc.nextInt();
 
-        if (num % 400 == 0 || num % 4 == 0 && num % 100 != 0){
+        if (num % 400 == 0 || (num % 4 == 0 && num % 100 != 0)){
             System.out.println("This is leap lear");
         }
         else {
