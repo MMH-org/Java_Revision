2183302d1f05922c6f70465442c1db1ec189e118
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-20 16:09:36 +0530

    print Pattern With help of \n

--- src/ProgrammingChallenge/BackSlacNTypePattern.java
+++ src/ProgrammingChallenge/BackSlacNTypePattern.java
@@ -2,7 +2,9 @@ package src.ProgrammingChallenge;
 
 public class BackSlacNTypePattern {
     public static void main(String[] args) {
-        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *");
-        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*");
+        System.out.println("*\n* *\n* * *\n* * * *\n* * * * *\n");
+        System.out.println("* * * * *\n* * * *\n* * *\n* *\n*\n");
+        System.out.println("        *\n      * *\n    * * *\n  * * * *\n* * * * *\n");
+
     }
 }
