36b591ba2b76b2ad629f7c667c6084421eb09fab
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-20 22:21:00 +0530

    Greater Number

--- src/ProgrammingChallenge/GreaterNumber.java
+++ src/ProgrammingChallenge/GreaterNumber.java
@@ -1,7 +1,22 @@
 package src.ProgrammingChallenge;
 
+import java.util.Scanner;
+
 public class GreaterNumber {
     public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter three number A ,B and C :  ");
+        int a = sc.nextInt();
+        int b = sc.nextInt();
+        int c = sc.nextInt();
 
+        if (a>=b && a>=c){
+            System.out.println("A is greater "+ a);
+        } else if (b>=c) {
+            System.out.println("B is greater" + b);
+        }
+        else {
+            System.out.println("C is greater" + c);
+        }
     }
 }
