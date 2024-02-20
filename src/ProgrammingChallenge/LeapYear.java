74152d03a30a7732760e9547b6bd4263e0ece484
Mohsin <mdmohsinhaider6@gmail.com>
2024-02-20 22:57:24 +0530

    leap year

---
+++ src/ProgrammingChallenge/LeapYear.java
@@ -0,0 +1,18 @@
+package src.ProgrammingChallenge;
+
+import java.util.Scanner;
+
+public class LeapYear {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Year which is leap year or Not ? :  ");
+        int num = sc.nextInt();
+
+        if (num % 400 == 0 || num % 4 == 0 && num % 100 != 0){
+            System.out.println("This is leap lear");
+        }
+        else {
+            System.out.println("Not leap year");
+        }
+    }
+}
