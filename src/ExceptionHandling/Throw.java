ad8a6256133e65268fd917ca080e4a55587c0425
Mohsin <mdmohsinhaider6@gmail.com>
2024-03-03 17:18:58 +0530

    user define throw define exception handling

--- src/ExceptionHandling/Throw.java
+++ src/ExceptionHandling/Throw.java
@@ -1,7 +1,13 @@
 package src.ExceptionHandling;
 
 public class Throw {
-    public static void main(String[] args) {
-
+    public static void main(String[] args){
+        int age=10;
+        if (age<=18){
+        throw new ArithmeticException ("hey dear is this is wrong age ");
+        }
+        else {
+            System.out.println("You are eligible");
+        }
     }
 }
