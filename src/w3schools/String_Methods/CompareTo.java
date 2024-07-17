98163afb25ad2a3d8cefe5878252d66de629fb28
Mohsin <mdmohsinhaider6@gmail.com>
2024-07-17 16:44:35 +0530

    Java String Methods

--- src/w3schools/String_Methods/CompareTo.java
+++ src/w3schools/String_Methods/CompareTo.java
@@ -2,8 +2,13 @@ package src.w3schools.String_Methods;
 
 public class CompareTo {
     public static void main(String[] args) {
-        String myStr1 = "Hell";
+        String myStr1 = "Hello";
         String myStr2 = "Hello";
         System.out.println(myStr1.compareTo(myStr2)); // Returns 0 because they are equal
+
+//        case two if difference
+        String myStr3 = "Hell";
+        String myStr4 = "Hello";
+        System.out.println(myStr3.compareTo(myStr4)); // Returns 0 because they are equal
     }
 }
