63e83bd5ec00cafdb1df50891115ec124e1dbdc5
Mohsin <mdmohsinhaider6@gmail.com>
2024-07-18 18:12:28 +0530

    Java String compareToIgnoreCase Method

---
+++ src/w3schools/String_Methods/CompareToIgnoreCase.java
@@ -0,0 +1,9 @@
+package src.w3schools.String_Methods;
+
+public class CompareToIgnoreCase {
+    public static void main(String[] args) {
+        String name1 = "MOHSIN";
+        String name2 = "mohsin";
+        System.out.println(name1.compareToIgnoreCase(name2));
+    }
+}
