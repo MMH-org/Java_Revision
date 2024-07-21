dc680daa146811e2223acfa3897b2a6609e8a0ed
Mohsin <mdmohsinhaider6@gmail.com>
2024-07-21 21:28:20 +0530

    Statatic array

---
+++ src/Array/StaticArray.java
@@ -0,0 +1,8 @@
+package src.Array;
+
+public class StaticArray {
+    public static void main(String[] args) {
+        int []arr = {1,2,3,57,65,55,5};
+        System.out.println(arr[2]);
+    }
+}
