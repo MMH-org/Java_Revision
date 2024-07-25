4283733765b0adcabf1ded1be9f8bc05fa0cd028
Mohsin <mdmohsinhaider6@gmail.com>
2024-07-25 13:02:57 +0530

    join isEmpty

---
+++ src/w3schools/String_Methods/IsEmpty.java
@@ -0,0 +1,10 @@
+package src.w3schools.String_Methods;
+
+public class IsEmpty {
+    public static void main(String[] args) {
+        String my_first_string = "Mohsin";
+        String my_second_string = "";
+        System.out.println(my_first_string.isEmpty());
+        System.out.println(my_second_string.isEmpty());
+    }
+}
---
+++ src/w3schools/String_Methods/Join.java
@@ -0,0 +1,8 @@
+package src.w3schools.String_Methods;
+
+public class Join {
+    public static void main(String[] args) {
+        String fruits = String.join(" ", "Orange", "Apple", "Mango"," mohsin");
+        System.out.println(fruits);
+    }
+}
