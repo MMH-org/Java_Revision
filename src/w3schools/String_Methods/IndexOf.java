34091df06ffef6fca79ffcfed39e225bbad63ea1
Mohsin <mdmohsinhaider6@gmail.com>
2024-07-25 12:23:51 +0530

    indexOf hashCode

---
+++ src/w3schools/String_Methods/IndexOf.java
@@ -0,0 +1,9 @@
+package src.w3schools.String_Methods;
+
+public class IndexOf {
+    public static void main(String[] args) {
+        String intro = "Hello Md Mohsin haider How are you";
+        System.out.println(intro.indexOf("Mohsin"));
+        System.out.println(intro.indexOf('M'));
+    }
+}
